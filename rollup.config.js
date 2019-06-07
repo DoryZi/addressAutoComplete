9d8618d2ec1e958525ef879d32096d82acabd9cb
Dory Zidon <dory.zidon@gmail.com>
2019-06-07 12:45:36 -0400

    working address auto-complete

---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import babel from "rollup-plugin-babel"
+import cleanup from "rollup-plugin-cleanup"
+import nodeResolve from 'rollup-plugin-node-resolve'
+import { uglify } from "rollup-plugin-uglify"
+import css from "rollup-plugin-css-porter"
+import commonjs from 'rollup-plugin-commonjs'
+
+
+export default [
+  {
+    input: "src/uspsAddressAutocomplete.js",
+    output: {
+      file: 'dist/uspsAddressAutocomplete.js',
+      name: "uspsAddressAutocomplete",
+      format: "umd"
+    },
+    plugins: [
+      nodeResolve({
+        jsNext : true
+      }),
+      commonjs({
+          include: 'node_modules/**',
+      }),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/proposal-class-properties"]
+      }),
+      //uglify(),
+      css({
+        raw: 'src/uspsAddressAutocomplete.css',
+        minified: 'dist/uspsAddressAutocomplete.css.min.css',
+      })
+    ]
+  },
+  {
+    input: "src/uspsAddressAutocomplete.js",
+    output: {
+      file: 'dist/uspsAddressAutocomplete.js',
+      name: "uspsAddressAutocomplete",
+      format: "umd"
+    },
+    plugins: [
+      nodeResolve({
+        jsNext : true
+      }),
+      commonjs({
+        include: 'node_modules/**',
+      }),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/proposal-class-properties"]
+      }),
+      //uglify(),
+      css({
+        raw: 'src/uspsAddressAutocomplete.css',
+        minified: 'dist/uspsAddressAutocomplete.css.min.css',
+      }),
+      cleanup()
+    ]
+  }
+];
---
+++ src/uspsAddressAutocomplete.js
@@ -0,0 +1,94 @@
+import autocomplete from 'autocompleter'
+import './uspsAddressAutocomplete.css'
+
+const DEBOUNCE = 150
+const REQUIRE_CONFIG_PARAMS = ['publicKey', 'selector', 'onSelect']
+
+export default class uspsAddressAutoComplete {
+  constructor(config) {
+    this.validateRequired(config)
+
+
+    config.debounceWaitMs = config.debounce || DEBOUNCE
+    config.input = document.querySelector(config.selector)
+    config.fetch = this.getSuggestions
+    config.emptyMsg = config.emptyMsg || "No suggestions found."
+    config.render = this.renderRow
+    this.callerOnSelect = config.onSelect
+    config.onSelect = this.onSelect
+    this.autoComplete = new autocomplete(config)
+    this.publicKey = config.publicKey
+    this.xhr = new XMLHttpRequest();
+    delete config.publicKey
+  }
+
+  validateRequired(config) {
+    for (const key of REQUIRE_CONFIG_PARAMS) {
+      if (!config[key]) {
+        throw new Error(`must provide ${key} in config, cannot run without it`)
+      }
+    }
+
+  }
+
+  getSuggestions = (inputText, update) => {
+    if (!this.autoComplete) {
+      return []
+    }
+    const query = encodeURIComponent(inputText)
+    const url = `https://us-autocomplete.api.smartystreets.com/suggest?auth-id=${this.publicKey}&prefix=${query}`
+    this.getUrl(url, (error, result) => {
+      if (error) {
+        throw new Error(error)
+      }
+      update(result.suggestions)
+    })
+
+  }
+
+  getUrl(url, cb) {
+    this.xhr.abort()
+    this.xhr.onload = () => {
+      if (this.xhr.status >= 200 && this.xhr.status < 300) {
+        cb(null,JSON.parse(this.xhr.response))
+      } else {
+        cb(this.xhr)
+      }
+      // Code that should run regardless of the request status
+    }
+    this.xhr.open('GET', url)
+    this.xhr.send()
+  }
+
+  renderRow = (item, query) => {
+    const rowContainer = document.createElement('div')
+    rowContainer.className = 'address-ac-item'
+    const icon = document.createElement('span')
+    icon.className = 'map-icon'
+    rowContainer.appendChild(icon)
+    const address1 = document.createElement('span')
+    address1.className = 'address1'
+    address1.innerText = item.street_line
+    rowContainer.appendChild(address1)
+    const text = document.createTextNode(item.text.substring(item.street_line.length))
+    rowContainer.appendChild(text)
+    return rowContainer
+  }
+
+  onSelect = (item) => {
+    this.getUrl(
+      `https://us-street.api.smartystreets.com/street-address?auth-id=${this.publicKey}&street=${encodeURIComponent(item.street_line)}&city=${encodeURIComponent(item.city)}&state=${encodeURIComponent(item.state)}`,
+      (error, results) => {
+        if (!results || !results.length) {
+          alert('selected address has no deliverable address, select another address please')
+          return
+        }
+        this.callerOnSelect(results)
+      }
+    )
+  }
+}
+
+
+
+
